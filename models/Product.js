9046f11f28c829a4c7c94b86754ca8c0c92cb0f9
AustinJoo97 <austinjoo97@gmail.com>
2021-05-10 14:34:36 -0400

    added foreign keys to productTag and Product; onDelete functions for all category-product relationships added

--- models/Product.js
+++ models/Product.js
@@ -33,6 +33,13 @@ Product.init(
       validate: {
         isNumeric: true
       }
+    },
+    reader_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
   },
   {
